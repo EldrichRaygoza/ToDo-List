5ad99eef151127c096c3ac56ebad8a006e53a143
Eldrich Raygoza <raygozaeldrich@gmail.com>
2025-02-05 18:44:04 -0600

    Componentes de TODO Machine

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItems(){
+  return(
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export {TodoItems};
---
+++ src/TodoTitle.js
@@ -0,0 +1,9 @@
+function TodoTitle(){
+    return(
+      <h1>
+        Has completado 3 de 5 TODOs
+      </h1>
+    );
+  }
+
+export {TodoTitle};
