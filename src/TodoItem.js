33a311943924bac988028eb13189926ae2609034
Eldrich Raygoza <raygozaeldrich@gmail.com>
2025-02-20 21:39:32 -0600

    Iconos en React

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,23 @@
+import {CompleteIcon} from './CompleteIcon'
+import {DeleteIcon} from './DeleteIcon'
 import './TodoItem.css'
 
 function TodoItems(props){
   return(
     <li className='Todo-Items'>
-    <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>V</span>
+    <CompleteIcon />
+    {/* <span 
+      className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+    >V</span> */}
     <p 
       onClick={props.onComplete} 
       className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}
     >{props.text}</p>
-    <span 
+    <DeleteIcon/>
+    {/* <span 
       className='Icon Icon-delete'
       onClick={props.onDelete}
-    >X</span>
+    >X</span> */}
     </li>
   );
 }
