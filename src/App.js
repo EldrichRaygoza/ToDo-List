922aed64b2b58d6d94d06006c3dc792fef1ffbf9
Eldrich Raygoza <raygozaeldrich@gmail.com>
2025-02-25 18:45:00 -0600

    localStorage y ajuste en el titulo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TodoList } from './TodoList';
 import { TodoItems } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const defaultTodos = [
+/* const defaultTodos = [
   {text: "Cortar cebolla", completed: true},
   {text: "Tomar el Curso de Intro a React.js", completed: false},
   {text: "Llorar con la Llorona", completed: false},
@@ -13,8 +13,27 @@ const defaultTodos = [
   {text: "Usar Estados Derivados", completed: true},
 ];
 
+const strTODOs = JSON.stringify(defaultTodos)
+
+localStorage.setItem('TODOs_V1', defaultTodos) */
+/* localStorage.removeItem('TODOs_V1', defaultTodos) */
+
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  /* Se obtiene los TODOs desde el localStorage */
+  const lS_TODOs = localStorage.getItem('TODOs_V1');
+  let parsedTodos;
+  
+  /* Verifica si existe un contenido en localStorage si no, crea la variable en un arreglo vacio */
+  if (!lS_TODOs) {
+    /* Importante que para crear contenido en el localStorage debe guardarse este en String */
+    localStorage.setItem('TODOs_V1', JSON.stringify([]))
+    parsedTodos = [];
+  } else{
+    /* El contenido del localStorage lo convierte en Objeto o este caso, un arreglo de objetos para la manipulacion de este en el codigo */
+    parsedTodos = JSON.parse(lS_TODOs);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -27,8 +46,16 @@ function App() {
     }
   );
 
+  /* Esta funcion guarda los cambios de los TODOs en el estado y en el localStorage */
+  const safeTodos = (newTodos) => {
+
+    localStorage.setItem('TODOs_V1', JSON.stringify(newTodos))
+
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
-    /* newTodos va copear el arrelgo de todos */
+    /* newTodos va copear el arreglo de todos */
     const newTodos = [...todos];
     /* Aqui se va buscar el indice del TODO donde se este ejecutando el evento */
     const todoIndex = newTodos.findIndex(
@@ -37,7 +64,7 @@ function App() {
     /* la propiedad completed del objeto cuyo indice encontramos va a alternar true/false cuando se ejecute el evento */
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     /* Se actualiza los TODOs con el nuevo arreglo */
-    setTodos(newTodos)
+    safeTodos(newTodos)
   }
   
   const deleteTodo = (text) => {
@@ -50,7 +77,7 @@ function App() {
     /* En base al indice encontrado va a eliminar este objeto con el metodo Splice, donde se coloca el indice donde va empezar a extraer/borrar y cuantos de ellos se eliminaran*/
     newTodos.splice(todoIndex, 1);
     /* Se actualiza los TODOs con el nuevo arreglo */
-    setTodos(newTodos)
+    safeTodos(newTodos)
   }
 
   return (
--- src/TodoTitle.js
+++ src/TodoTitle.js
@@ -1,17 +1,22 @@
 import './TodoTitle.css';
 
 function TodoTitle({total, completed}){
-    return(
+  return(
 
+    total === 0 ? 
+    <h1 className='TodoTitle'>
+      <span>😃✏️ Escribe tu primer TODO ✏️😃</span>
+    </h1>
+    :
       total === completed ? 
-      <h1 className='TodoTitle'>
-        <span>🎉🎉🎉 HAS COMPLETADO TODOs 🎉🎉🎉</span>
-      </h1>
+        <h1 className='TodoTitle'>
+          <span>🎉🎉🎉 HAS COMPLETADO TODOs 🎉🎉🎉</span>
+        </h1>
       :
-      <h1 className='TodoTitle'>
-        Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-      </h1>
-    );
-  }
+        <h1 className='TodoTitle'>
+          Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+        </h1>
+  );
+}
 
 export {TodoTitle};
